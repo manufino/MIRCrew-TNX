2eab2681cff7572fb1b25ec6a2d4dd5071740243
Manuel Finessi <fino.manu@gmail.com>
2024-06-03 13:00:12 +0200

    update files

--- MIRCrew TNX/content.js
+++ MIRCrew TNX/content.js
@@ -1,32 +1,27 @@
-// Funzione per creare il pulsante
 function createButton() {
     const button = document.createElement('button');
     button.innerText = 'Ringrazia';
-    button.style.padding = '20px 40px'; // Aumenta le dimensioni del pulsante
-    button.style.fontSize = '22px'; // Aumenta la dimensione del testo
+    button.style.padding = '20px 40px';
+    button.style.fontSize = '22px';
     button.style.backgroundColor = '#ffcc00';
     button.style.border = 'none';
     button.style.borderRadius = '5px';
     button.style.cursor = 'pointer';
-    button.style.boxSizing = 'border-box'; // Assicura che il bordo sia interno
-    button.style.color = 'black'; // Testo nero di default
+    button.style.boxSizing = 'border-box';
+    button.style.color = 'black';
 
-    // Aggiungi un effetto hover
     button.addEventListener('mouseover', () => {
         button.style.outline = '2px solid red';
         button.style.color = 'green';
     });
 
-    // Rimuovi l'effetto hover quando il mouse esce dal pulsante
     button.addEventListener('mouseout', () => {
         button.style.outline = 'none';
         button.style.color = 'black';
     });
 
-    // Aggiungi un'azione al pulsante
     button.addEventListener('click', () => {
-        // Memorizza la posizione dello scroll
-        scrollPosition = window.scrollY;
+        const scrollPosition = window.scrollY;
         localStorage.setItem('scrollPosition', scrollPosition);
         simulateLinkClick();
     });
@@ -34,19 +29,17 @@ function createButton() {
     return button;
 }
 
-// Verifica la presenza degli elementi specificati e li sostituisce
 function checkForElements() {
     const targetElements = document.querySelectorAll('dl.hidebox.hi dd center');
     targetElements.forEach(targetElement => {
         if (targetElement.innerHTML.includes('Ringrazia il Releaser cliccando sul tasto')) {
             const button = createButton();
-            targetElement.innerHTML = ''; // Sostituisce il contenuto dell'elemento target
+            targetElement.innerHTML = '';
             targetElement.appendChild(button);
         }
     });
 }
 
-// Funzione per simulare il clic su un link
 function simulateLinkClick() {
     const link = document.querySelector('a[id^="lnk_thanks_post"]');
     if (link) {
@@ -54,7 +47,6 @@ function simulateLinkClick() {
     }
 }
 
-// Ripristina la posizione dello scroll
 function restoreScrollPosition() {
     const savedPosition = localStorage.getItem('scrollPosition');
     if (savedPosition) {
@@ -63,7 +55,6 @@ function restoreScrollPosition() {
     }
 }
 
-// Assicura che lo script venga eseguito dopo il caricamento completo della pagina
 window.addEventListener('load', () => {
     checkForElements();
     restoreScrollPosition();
